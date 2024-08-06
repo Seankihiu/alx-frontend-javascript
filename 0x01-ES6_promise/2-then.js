4a712d27c702efa92b9751246d655773b887f585
Sean Kihiu <seankihiu45@gmail.com>
2024-08-06 04:17:23 -0700

    files

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,9 @@
-export default function handleResponseFromAPI(promise) {
+function handleResponseFromAPI(promise) {
+  const body = { status: 200, body: 'success' };
+
   return promise
-      .then(() => {
-          console.log('Got a response from the API');
-          return { status: 200, body: 'success' };
-      })
-      .catch(() => new Error())
-      .finally(() => console.log('Got a response from the API'));
+    .then(() => body)
+    .catch((error) => error)
+    .finally(() => console.log('Got a response from the API'));
 }
+export default handleResponseFromAPI;
--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,12 +1,13 @@
 export default function guardrail(mathFunction) {
   const queue = [];
+  let res;
   try {
-      const result = mathFunction();
-      queue.push(result);
+    res = mathFunction();
   } catch (error) {
-      queue.push(`Error: ${error.message}`);
-  } finally {
-      queue.push('Guardrail was processed');
+    res = `Error: ${error.message}`;
   }
+  queue.push(res);
+  queue.push('Guardrail was processed');
+
   return queue;
 }
